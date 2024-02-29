829635b9cccbe316c7c06c830c0fb20e5406e924
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-02-29 12:53:29 -0600

    Completed Lab

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,21 @@
-# your code goes here!
+import re
+
+class EmailAddressParser():
+    def __init__(self, emails):
+        self.emails = emails
+
+    @property
+    def emails(self):
+        return self._emails
+    
+    @emails.setter
+    def emails(self, emails):
+        if isinstance(emails, str):
+            self._emails = emails
+        else:
+            raise TypeError("Emails should be a string")
+    
+    def parse(self):
+        pattern = r'[A-z]+[A-z0-9._%+-]+@[A-z0-9.-]+\.[A-z]{2,}'
+        regex = re.compile(pattern)
+        return sorted(regex.findall(self._emails))
